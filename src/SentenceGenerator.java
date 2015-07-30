4c35f4dfa24d557cb6fd3942bd1939f7037aa5ad
Christopher Wood <christopherwood07@gmail.com>
2015-07-29 18:52:50 -0700

    Sentence updates

--- src/SentenceGenerator.java
+++ src/SentenceGenerator.java
@@ -5,5 +5,18 @@ import simplenlg.phrasespec.*;
 import simplenlg.features.*;
 
 public class SentenceGenerator {
-	// TODO
+	public static void main(String[] args) {
+		Lexicon lexicon = Lexicon.getDefaultLexicon();
+		NLGFactory nlgFactory = new NLGFactory(lexicon);
+		Realiser realiser = new Realiser(lexicon);
+
+		SPhraseSpec p = nlgFactory.createClause();
+		p.setSubject("Mary");
+		p.setVerb("chase");
+		p.setObject("the monkey");
+
+		String output2 = realiser.realiseSentence(p);
+		System.out.println(output2);
+	}
 }
+
