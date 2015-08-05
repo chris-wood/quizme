41d0c3b089484f0b9f06f6969dd4b8aee0c3bcff
Christopher Wood <christopherwood07@gmail.com>
2015-08-05 16:57:30 -0700

    Lev. distance recursive implementation

--- src/SentenceDistance.java
+++ src/SentenceDistance.java
@@ -3,10 +3,28 @@ public class SentenceDistance {
         // pass
     }
 
-    public int computeDistance(String a, String b) {
-        // TODO
+    private int levenshteinDistance(String a, String b, int i, int j) {
+        if (j == -1) {
+            return i;
+        } else if (i == -1) {
+            return j;
+        } else {
+            if (a[j] == b[i]) {
+                return levenschteinDistance(a, b, i - 1, j - 1);
+            } else {
+                int delDistance = levenschteinDistance(a, b, i - 1, j) + 1; // delete
+                int insDistance = levenschteinDistance(a, b, i, j - 1) + 1; // insert
+                int swapDistance = levenschteinDistance(a, b, i - 1, j - 1) + 1; // substitute
+                
+                int min = delDistance < insDistance ? delDistance : insDistance;
+                min = min < swapDistance ? min : swapDistance;
+                return min;
+            }
+        }
+    }    
 
-        return 0;
+    public int computeDistance(String a, String b) {
+        return levenschteinDistance(a, b, b.length(), a.length());
     }
 
     public static void main(String[] args) {
@@ -18,3 +36,4 @@ public class SentenceDistance {
         System.out.println("Distance between " + a + " and " + " b = " + distance);
     }
 }
+
