ea282186a5a344da75798beb6e1c210c10cb0495
Christopher Wood <christopherwood07@gmail.com>
2015-08-11 07:24:24 -0700

    random things

--- src/trans.sh
+++ src/trans.sh
@@ -1,1 +1,2 @@
-java -cp sqljet-1.1.10.jar:javax.json-api-1.0.jar:simplenlg-v4.4.2.jar:. Translator
+#!/bin/bash
+java -cp sqljet-1.1.10.jar:javax.json-api-1.0.jar:simplenlg-v4.4.2.jar:. Translator ../apikey ../apikey test
