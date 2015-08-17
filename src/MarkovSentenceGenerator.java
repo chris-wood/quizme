662378750768b5490995ad22830284fc13791714
Christopher Wood <christopherwood07@gmail.com>
2015-08-17 07:41:27 -0700

    Markov sentence generator stub

---
+++ src/MarkovSentenceGenerator.java
@@ -0,0 +1,13 @@
+public class MarkovSentenceGenerator extends SentenceGenerator {
+    public String createRandomNounSentence(double difficulty) {
+        return "";
+    }
+
+    public String createRandomFullSentence(double difficulty) {
+        return "";
+    }
+
+    public String createRandomPhrase(double difficulty) {
+        return "";
+    }
+}
