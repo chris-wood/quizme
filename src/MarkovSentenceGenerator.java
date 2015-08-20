9433e5654f05c774e6226c686be5d8392dde909d
Christopher Wood <christopherwood07@gmail.com>
2015-08-20 08:12:36 -0700

    Markov sentence generator using markov frequency

--- src/MarkovSentenceGenerator.java
+++ src/MarkovSentenceGenerator.java
@@ -1,4 +1,11 @@
 public class MarkovSentenceGenerator extends SentenceGenerator {
+
+    private WordFrequencyGenerator wfg;
+
+    public MarkovSentenceGenerator(String trainingFile) {
+        wfg = new WordFrequencyGenerator(trainingFile);
+    }
+
     public String createRandomNounSentence(double difficulty) {
         return "";
     }
