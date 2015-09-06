48d7fe151abad857784b313aa98324537de8b480
Christopher Wood <christopherwood07@gmail.com>
2015-09-06 09:28:33 -0700

    sentence generator from the web

---
+++ src/WebSentenceGenerator.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class WebSentenceGenerator extends SentenceGenerator {
+
+    public WebSentenceGenerator(String config) {
+
+    }
+
+    public String createRandomNounSentence(double difficulty) {
+        return "";
+    }
+
+    public String createRandomFullSentence(double difficulty) {
+        return "";
+    }
+
+    public String createRandomPhrase(double difficulty) {
+        return "";
+    }
+}
